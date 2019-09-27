4875546e19557b86b774845a5231e96cedc9a6d5
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-09-27 15:16:18 +0700

    set default beforeCall function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const procedure = require('./lib/procedure')
 const server = require('./lib/server')
 
+procedure.def('beforeCall', () => {});
+
 module.exports = {
   def: procedure.def,
   beforeCall: procedure.beforeCall,
