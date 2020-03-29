d2f86de7f26562d3942d68d18e322c881b75072b
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-03-29 16:35:08 +0700

    add injector on procedure call

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const server = require('./lib/server')
 procedure.def('beforeCall', () => {});
 
 module.exports = {
+  inject: procedure.inject,
   def: procedure.def,
   beforeCall: procedure.beforeCall,
   listen: server.listen,
